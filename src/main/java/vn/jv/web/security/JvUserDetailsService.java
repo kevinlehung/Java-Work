8fabea6323083492f610682ec1c3a4f6867e2619
517f72fa4382ec501000018d <lehung@ymail.com>
2014-01-11 08:54:38 +0700

    Update code

--- src/main/java/vn/jv/web/security/JvUserDetailsService.java
+++ src/main/java/vn/jv/web/security/JvUserDetailsService.java
@@ -10,15 +10,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import vn.jv.db.dao.IUserDAO;
 import vn.jv.db.entity.User;
 
-/**
- * Custom UserDetailsService which loads users from Jv database
- * 
- *
- */
+@Service
 public class JvUserDetailsService implements UserDetailsService {
 	@Autowired@Qualifier("userDAO")
 	private IUserDAO userDAO;
