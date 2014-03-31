a0c0558e47eb4c003708a1d56a36aa1e317bb8f0
517f72fa4382ec501000018d <hunglevn@outlook.com>
2014-03-31 13:37:47 +0700

    Enable webconfiguration for testing

--- src/test/java/vn/jv/BaseTester.java
+++ src/test/java/vn/jv/BaseTester.java
@@ -4,11 +4,13 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import vn.jv.persist.repositories.RepoConfig;
 import vn.jv.spring.context.CacheConfig;
 import vn.jv.spring.context.ServiceConfig;
 import vn.jv.spring.context.SpringConfig;
+import vn.jv.web.config.WebConfig;
 
 /**
  *
@@ -16,8 +18,8 @@ import vn.jv.spring.context.SpringConfig;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-//@WebAppConfiguration
-@ContextConfiguration(loader= AnnotationConfigContextLoader.class, classes = {/*WebConfig.class, */SpringConfig.class, CacheConfig.class, RepoConfig.class, ServiceConfig.class})
+@WebAppConfiguration
+@ContextConfiguration(loader= AnnotationConfigContextLoader.class, classes = {WebConfig.class, SpringConfig.class, CacheConfig.class, RepoConfig.class, ServiceConfig.class})
 public class BaseTester {
 
 }
