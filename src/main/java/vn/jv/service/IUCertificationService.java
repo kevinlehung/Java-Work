bb482005b4050f10db3bbcc4581e50d24c56f8a0
hieutran83 <tthieucdl@gmail.com>
2014-03-31 23:46:08 +0700

    + Implement UCertification CRUD at Service layer

--- src/main/java/vn/jv/service/IUCertificationService.java
+++ src/main/java/vn/jv/service/IUCertificationService.java
@@ -1,16 +1,26 @@
 package vn.jv.service;
 
+import java.util.Date;
 import java.util.List;
 
 import vn.jv.persist.domain.UCertification;
+import vn.jv.persist.domain.User;
 
 /**
  * Contain operations related to UCertification
  * 
- * @author hieutran83
+ * @author tthieucdl@gmail.com
  *
  */
 public interface IUCertificationService extends IBaseService{
 
 	public List<UCertification> findByUserId(int userId);
+	
+	public void create(User user, String conferringOrganization, String professionalCertificate,
+						Date dateAwarded, String certificateNumber, String description);
+	
+	public void update(int uCertificationId, String conferringOrganization, String professionalCertificate,
+			Date dateAwarded, String certificateNumber, String description);
+	
+	public void delete(int uCertificationId);
 }
--- src/main/java/vn/jv/service/UCertificationService.java
+++ src/main/java/vn/jv/service/UCertificationService.java
@@ -1,8 +1,10 @@
 package vn.jv.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,7 @@ import vn.jv.persist.repositories.UCertificationRepo;
 /**
  * Contain operations related to UCertification
  * 
- * @author hieutran83
+ * @author tthieucdl@gmail.com
  *
  */
 @Service("uCertificationService")
@@ -22,8 +24,41 @@ public class UCertificationService extends BaseService implements IUCertificatio
 	@Autowired
 	UCertificationRepo uCertificationRepo;
 	
-	@Cacheable(value = "UCertificationService.findByUserId", key="#userId")
+	//@Cacheable(value = "UCertificationService.findByUserId", key="#userId")
 	public List<UCertification> findByUserId(int userId) {
 		return uCertificationRepo.findByUser(new User(userId));
+	}
+	
+	@CacheEvict(value = "UCertificationService.findByUserId", key="#userId")
+	public void create(User user, String conferringOrganization, String professionalCertificate,
+			Date dateAwarded, String certificateNumber, String description) {
+		
+		UCertification uCertification = new UCertification();
+		uCertification.setUser(user);
+		uCertification.setConferringOrganization(conferringOrganization);
+		uCertification.setProfessionalCertificate(professionalCertificate);
+		uCertification.setDateAwarded(dateAwarded);
+		uCertification.setCertificateNumber(certificateNumber);
+		uCertification.setDescription(description);
+		
+		uCertificationRepo.save(uCertification);	
+	}
+		
+	
+	public void update(int uCertificationId, String conferringOrganization, String professionalCertificate,
+						Date dateAwarded, String certificateNumber, String description) {
+		UCertification uCertification = uCertificationRepo.findOne(uCertificationId);
+		// Must check null here or throw item not found? Later
+		uCertification.setConferringOrganization(conferringOrganization);
+		uCertification.setProfessionalCertificate(professionalCertificate);
+		uCertification.setDateAwarded(dateAwarded);
+		uCertification.setCertificateNumber(certificateNumber);
+		uCertification.setDescription(description);
+		
+		uCertificationRepo.save(uCertification);
+	}
+	
+	public void delete(int uCertificationId) {
+		uCertificationRepo.delete(uCertificationId);
 	}
 }
