efe80efdab404b7373cf23da8e8d08b54057aee8
RickCP <lvhuy08t2@gmail.com>
2014-04-01 10:17:10 +0700

    Add repository of t_user_test table.

---
+++ src/main/java/vn/jv/persist/repositories/TUserTestCustomRepo.java
@@ -0,0 +1,10 @@
+package vn.jv.persist.repositories;
+
+/**
+ * 
+ * @author lvhuy08t2@gmail.com
+ * 
+ */
+public interface TUserTestCustomRepo {
+
+}
---
+++ src/main/java/vn/jv/persist/repositories/TUserTestRepo.java
@@ -0,0 +1,14 @@
+package vn.jv.persist.repositories;
+
+import vn.jv.persist.BaseRepo;
+import vn.jv.persist.domain.TUserTest;
+
+/**
+ *
+ * @author lvhuy08t2@gmail.com
+ *
+ */
+public interface TUserTestRepo  extends BaseRepo<TUserTest, Integer> , TUserTestCustomRepo {
+	
+
+}
