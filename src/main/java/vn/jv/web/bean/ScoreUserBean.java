be015be838a98124b415ba8a2ba3b6ddf5939fa9
RickCP <lvhuy08t2@gmail.com>
2014-04-02 11:51:29 +0700

    Add ScoreUserBean

---
+++ src/main/java/vn/jv/web/bean/ScoreUserBean.java
@@ -0,0 +1,36 @@
+package vn.jv.web.bean;
+/**
+ *
+ * @author lvhuy08t2@gmail.com
+ *
+ */
+public class ScoreUserBean {
+	private Integer maxScore;
+	private Integer skillId;
+	private String name;
+
+	public Integer getMaxScore() {
+		return maxScore;
+	}
+
+	public void setMaxScore(Integer maxScore) {
+		this.maxScore = maxScore;
+	}
+
+	public Integer getSkillId() {
+		return skillId;
+	}
+
+	public void setSkillId(Integer skillId) {
+		this.skillId = skillId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
